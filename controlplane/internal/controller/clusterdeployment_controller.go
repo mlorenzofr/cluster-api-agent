c34882412ef6f9dc5efe7b174e7e122c5e0aaa45
Manuel Lorenzo <mlorenzofr@redhat.com>
2025-01-29 13:02:29 +0100

    Add new capabilities to Openshift installation

--- controlplane/internal/controller/clusterdeployment_controller.go
+++ controlplane/internal/controller/clusterdeployment_controller.go
@@ -251,7 +251,7 @@ func (r *ClusterDeploymentReconciler) computeAgentClusterInstall(
 		aci.Spec.IngressVIPs = acp.Spec.Config.IngressVIPs
 		aci.Spec.PlatformType = hiveext.PlatformType(configv1.BareMetalPlatformType)
 		aci.Annotations = map[string]string{
-			InstallConfigOverrides: `{"capabilities": {"baselineCapabilitySet": "None", "additionalEnabledCapabilities": ["baremetal","Console","Insights","OperatorLifecycleManager","Ingress"]}}"`,
+			InstallConfigOverrides: `{"capabilities": {"baselineCapabilitySet": "None", "additionalEnabledCapabilities": ["baremetal","Console","Insights","OperatorLifecycleManager","Ingress","CSISnapshot","marketplace"]}}"`,
 		}
 	}
 	return aci, nil
